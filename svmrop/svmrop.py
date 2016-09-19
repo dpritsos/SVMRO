604b7517194c82c30fafd5ea956cf4f03314f911
Dimitrios Pritsos <dpritsos@extremepro.gr>
2016-09-19 20:35:14 +0300

    Ready to rock.

--- svmrop/svmrop.py
+++ svmrop/svmrop.py
@@ -36,9 +36,6 @@ class LinearSetSVM(object):
 
     def optimize(self, X, yp_i, yn_i, yu_i):
 
-        # Making an array to matrix if not already.
-        #X = np.matrix(X)
-
         # Training the Linear SVM, either one-class or binary
         if self.svm_type == 'oneclass':
 
@@ -52,32 +49,20 @@ class LinearSetSVM(object):
             self.lsvm.fit(X[np.hstack((yp_i, yn_i)), :], y)
 
         # Getting preditions of the LinearSVM for all the sampels, Postive, Negative, Uknown.
-        pds = self.lsvm.decision_function(X)
+        self.pds = self.lsvm.decision_function(X)
+        if self.svm_type == 'oneclass':
+            self.pds = np.transpose(self.pds)[0]
 
         # Sorting the predicted distances.
-        pds_idxs = np.argsort(pds)
+        pds_idxs = np.argsort(self.pds)
         inv_pds_idxs = pds_idxs[::-1]
 
         # Getting index in the pds vector of the min and max distances, more or less...
         # equivalent to candidate SVs for the Greedy Optimization that follows. Then setting...
         # ...them as the initill Close and Far Hyperplane intial ofset position form the...
         # ...SVM hyperplane/decision function.
-        self.near_H_i = np.argmin(pds)
-        self.far_H_i = np.argmax(pds)
-
-        # Getting the Decision Hyperplane's Normal vector.
-        # self.N_vect = np.matrix(self.lsvm.coef_)
-
-        # Getting the Normal vector of the most distaned parrale hyperplane from the...
-        # ...decision hyperplane.
-        # max_ds_x = X[max_ds_i, :]
-        # proj_n_max_dsmpl = ((max_ds_x*self.near_H.T) / np.linalg.norm(self.near_H.T)) *...
-        # self.near_H
-        # self.far_H = np.cross(proj_n_max_dsmpl, max_ds_x)
-
-        # Getting the intercept of the decision function, a.k.a the decision hyperplane.
-        # incpt = lsvm.intercept_
-        # I probably won't use this.
+        self.near_H_i = np.argmin(self.pds)
+        self.far_H_i = np.argmax(self.pds)
 
         # Starting Greedy Optimization process
         min_Risk = np.Inf
@@ -85,8 +70,8 @@ class LinearSetSVM(object):
             for inv_pds_i in inv_pds_idxs[1::]:
 
                 # Moving the Hyperplanes.
-                new_near = pds[pds_i]
-                new_far = pds[inv_pds_i]
+                new_near = self.pds[pds_i]
+                new_far = self.pds[inv_pds_i]
 
                 # ## Calculating Constraintes ##
                 p4y_pos = self.predictions(X[yp_i], new_near, new_far)
@@ -162,29 +147,32 @@ class LinearSetSVM(object):
                         self.near_H_i = pds_i
                         self.far_H_i = inv_pds_i
 
-        return pds[self.near_H_i], pds[self.far_H_i]
+        return self.pds[self.near_H_i], self.pds[self.far_H_i]
 
     def refine_planes(self, yp_i, yn_i):
-
-        #
-        pds = self.lsvm.decision_function(X)
 
         # BE CAREFULL self.dfunc(X[yp_i], new_near, new_far) OR
         ds_posz = np.sum(self.lsvm.decision_function(X[yp_i]))
 
         if self.near_H_i > 0:
-            near_H = pds[self.near_H_i]*(0.5 - self.mrgn_nw) +\
-                pds[self.near_H_i - 1]*(self.mrgn_nw - 0.5)
+            near_H = self.pds[self.near_H_i]*(0.5 - self.mrgn_nw) +\
+                self.pds[self.near_H_i - 1]*(self.mrgn_nw - 0.5)
         else:
             near_H = np.min(pds) - self.mrgn_nw*ds_posz
 
         if self.far_H_i < (yp_i.shape[0] + yn_i.shape[0]):
-            far_H = pds[self.far_H_i]*(0.5 - self.mrgn_fw) +\
-                pds[self.far_H_i + 1]*(self.mrgn_fw - 0.5)
+            far_H = self.pds[self.far_H_i]*(0.5 - self.mrgn_fw) +\
+                self.pds[self.far_H_i + 1]*(self.mrgn_fw - 0.5)
         else:
-            far_H = np.max(pds) - self.mrgn_fw*ds_posz
+            far_H = np.max(self.pds) - self.mrgn_fw*ds_posz
 
         return near_H, far_H
+
+    def predict(self, X, near_H, far_H):
+
+        dsz_N, dsz_F = self.dfunc(X, near_H, far_H)
+
+        return np.where(((dsz_N >= 0) & (dsz_F >= 0)), 1, -1), np.hstack((dsz_N, dsz_F))
 
     def predictions(self, X, near_H, far_H):
 
@@ -195,6 +183,8 @@ class LinearSetSVM(object):
     def dfunc(self, X, near_H, far_H):
 
         dsz_X = self.lsvm.decision_function(X)
+        if self.svm_type == 'oneclass':
+            dsz_X = np.transpose(dsz_X)[0]
 
         dsz_N = far_H - dsz_X
         dsz_F = dsz_X - near_H
@@ -238,4 +228,18 @@ if __name__ == '__main__':
 
     print 'Data set X:', X
     print 'Expected Y:', y
-    print 'Predicted Y', llsvm.predictions(X, near_H, far_H)
+    print 'Predicted Y', llsvm.predict(X, near_H, far_H)
+
+    # Getting the Decision Hyperplane's Normal vector.
+    # self.N_vect = np.matrix(self.lsvm.coef_)
+
+    # Getting the Normal vector of the most distaned parrale hyperplane from the...
+    # ...decision hyperplane.
+    # max_ds_x = X[max_ds_i, :]
+    # proj_n_max_dsmpl = ((max_ds_x*self.near_H.T) / np.linalg.norm(self.near_H.T)) *...
+    # self.near_H
+    # self.far_H = np.cross(proj_n_max_dsmpl, max_ds_x)
+
+    # Getting the intercept of the decision function, a.k.a the decision hyperplane.
+    # incpt = lsvm.intercept_
+    # I probably won't use this.
