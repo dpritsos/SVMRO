98a5646ab5356c91771fbcaab78935e8d612770b
Dimitrios Pritsos <dpritsos@extremepro.gr>
2018-08-27 14:39:10 +0300

    Python packaging enabled

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="SVMROp",
+    version="0.0.1",
+    author="Dimitrios Pritsos",
+    author_email="dpritsos@extremepro.gr",
+    description="",
+    long_description="",
+    long_description_content_type="",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: GPL License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
